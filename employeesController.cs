74c54da950d1dc57e11f6f2299f7ecb3d7135370
comkar999 <31873952+comkar999@users.noreply.github.com>
2017-09-30 15:55:19 -0500

    Add files via upload

---
+++ employeesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class employeesController : ApiController
+    {
+
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDBEntities edb = new EmployeeDBEntities())
+            {
+                return edb.Employees.ToList();
+            }
+        }
+
+        //public void put([FromUri]int id,[FromUri] Employee emp)
+        //{
+        //    using (EmployeeDBEntities edb= new EmployeeDBEntities())
+        //    {
+        //        Employee emplo =  edb.Employees.Single(x => x.ID == id);
+        //        emplo.FirstName = emp.FirstName;
+        //        emplo.LastName = emp.LastName;
+        //        emplo.Gender = emp.Gender;
+        //        emplo.Salary = emp.Salary;
+        //        edb.SaveChanges();                 
+
+        //    }
+        //}
+       
+    }
+}
